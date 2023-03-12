0f325c1ce62d3e5890b4e18fa171c419b29d8125
Yash Wardhan <yash1234sammie@gmail.com>
2023-03-12 11:27:54 -0400

    first

---
+++ data.js
@@ -0,0 +1,31 @@
+data = {
+    "about": [],
+    "education":[
+        {
+            "school": "Rutgers, The State University of New Jersey",
+            "location": "New Brunswick, New Jersey, USA",
+            "from": "September 2022",
+            "to": "May 2024",
+            "degree": "Master of Science",
+            "major": "Computer Science",
+            "minor": "",
+        },{
+            "school": "Manipal Institute of Technology",
+            "location": "Manipal, Karnataka, India",
+            "from": "July 2018",
+            "to": "June 2022",
+            "degree": "Bachelor of Technology",
+            "major": "Information Tecgnology",
+            "minor": "Data Analytics",
+        },{
+            "school": "The Lawrence School, Sanawar",
+            "location": "Solan, Himachal Pradesh, India",
+            "from": "February 2009",
+            "to": "Deceber 2016",
+            "degree": "High School",
+            "major": "",
+            "minor": "",
+        }],
+    "internships": [],
+    "projects": [],
+}
---
+++ function.js
@@ -0,0 +1,4 @@
+document.getElementById('navbar-btn').onclick = ()=>{
+    document.getElementById('navbar').classList.toggle("sidebar-active");
+    document.getElementById('navbar').classList.toggle("sidebar-inactive");
+}
