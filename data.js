551cd4adb6cff506a36967c05615ff02ef60f9e3
Yash Wardhan <yash1234sammie@gmail.com>
2023-03-16 02:11:08 -0400

    bg

--- data.js
+++ data.js
@@ -4,28 +4,90 @@ data = {
         {
             "school": "Rutgers, The State University of New Jersey",
             "location": "New Brunswick, New Jersey, USA",
-            "from": "September 2022",
-            "to": "May 2024",
+            "from": "Aug '22",
+            "to": "May '24",
             "degree": "Master of Science",
             "major": "Computer Science",
             "minor": "",
+            "logo": "rutgers.png",
         },{
             "school": "Manipal Institute of Technology",
             "location": "Manipal, Karnataka, India",
-            "from": "July 2018",
-            "to": "June 2022",
+            "from": "Jul '18",
+            "to": "Jun '22",
             "degree": "Bachelor of Technology",
-            "major": "Information Tecgnology",
+            "major": "Information Technology",
             "minor": "Data Analytics",
+            "logo": "manipal.png",
         },{
             "school": "The Lawrence School, Sanawar",
             "location": "Solan, Himachal Pradesh, India",
-            "from": "February 2009",
-            "to": "Deceber 2016",
-            "degree": "High School",
-            "major": "",
+            "from": "Feb '09",
+            "to": "Dec '16",
+            "degree": "Higher Secondary Education",
+            "major": "PCM with Economics",
             "minor": "",
+            "logo": "sanawar.png",
         }],
-    "internships": [],
-    "projects": [],
+    "internships": [
+        {
+            "company": "TCS Digital",
+            "role": "Frontend Developer Intern",
+            "location": "India (Remote)",
+            "from": "Mar '22",
+            "to": "Jun '22",
+            "logo": "tcs.png",
+            "desc": ["Built web pages and functionalities to migrate from an outdated platform using Angular Material to incorporate Material Design that provides better UX and high-quality digital experience on the web."],
+        },{
+            "company": "Rtwo Healthcare Solutions ",
+            "role": "Software Engineer Intern",
+            "location": "Bangalore, Karnataka, India",
+            "from": "Jan '22",
+            "to": "May '22",
+            "logo": "rtwo.png",
+            "desc":[
+                "Programmed full stack web solution using MERN stack to simplify patient admission and device allocation by employing IoT devices to interact with server, reducing human error.",
+                "Designed and deployed a network of esp32 microcontroller along with required API routes that establishes communication with server to facilitate data access."],
+        },{
+            "company": "Grroom",
+            "role": "Software Developer Intern",
+            "location": "India (Remote)",
+            "from": "May '21",
+            "to": "Jul '21",
+            "logo": "grroom.png",
+            "desc": [
+                "Constructed tailored recommendation algorithm to support search bar drop-down with product filters to deliver query results matching available catalogued products.",
+                "Implemented codes to automate data scraping for thousands of products from each website for listing on the app.",
+                "Wrote programs to improve efficiency in manual data labeling and managed MongoDB database for training data."]
+        }],
+    "projects": [
+        {
+            "title": "Advanced Encryption Standard",
+            "from": "Oct '22",
+            "to": "Nov '22",
+            "desc":["Built an AES package having primary four functions and corresponding inverse functions performed in each round of AES encryption and decryption utilizing the key and other parameters taken from the user."]
+        },{
+            "title": "Unix System Core Libraries' Implementation",
+            "from": "Oct '22",
+            "to": "Oct '22",
+            "desc":["Replicated the implementation of a file system and memory allocation that performs data manipulation in memory by creating custom functionalities for standard operations."]
+        },{
+            "title": "Face Mask Detector",
+            "from": "Oct '22",
+            "to": "Oct '22",
+            "desc":["Constructed a model using YOLOv3 that categorizes individuals not wearing face masks in real time by transferring learned parameters and further training with a customized data set."]
+        },{
+            "title": "Travel Journal - Android and Web Application",
+            "from": "Oct '22",
+            "to": "Oct '22",
+            "desc":["Developed a cross-platform social media app using Android Studio, Node.js, and MongoDB enabling users to post personal data to track travel history"]
+        },{
+            "title": "Project Autonomous Underwater Vehicle Manipa",
+            "from": "Oct '22",
+            "to": "Oct '22",
+            "desc":[
+                "Implemented ROS to enable AUV components to communicate with each other in required format with minimal latency and error, allowing components to collaborate and complete assigned tasks.",
+                "Studied the working of convolutional neural networks (CNN) and YOLO with computer vision team to implement a model for the detection and classification of objects underwater in real-time." ],
+        }
+    ],
 }
--- populate.js
+++ populate.js
@@ -1,14 +1,27 @@
+var bgIconList = ["github_b.png", "gmail_b.png", "insta_b.png", "linkedin_b.png", "whatsapp_b.png" ];
+
+// 30 icons needed
+var icon_row ="<div>";
+for(var rep=0; rep<6; rep++){
+    for (let i = 0; i < bgIconList.length; i++)
+    icon_row += '<img src="icons/dock_icons/' + bgIconList[i] + '">';
+}
+icon_row += "</div>";
+$('.bg_row').html(icon_row + icon_row);
+
+var row = $('.bg_section').html(), bg = "";
+for(var i=0; i<24; i++) bg += row;
+$('.bg_section').html(bg);
 
 
 data.education.forEach(function (data, idx){
     var logo = '<div class="card_logo_box">';
     logo += '<div class="card_logo">';
-    logo += '<img src="dock_icons/' + "gmail_b.png" + '">';
+    logo += '<img src="icons/logos/' + data.logo + '">';
     logo += '</div>';
     logo += '</div>';
 
     var content = '<div class="timeline_item">';
-
     content += '<div class="card_info">';
     if(idx%2===0) content += logo;
     content += '<div class="card_date">' + data.from + " - " + data.to + '</div>';
@@ -17,46 +30,72 @@ data.education.forEach(function (data, idx){
 
     content += '<div class="card_item">';
     content += '<div class="card_title">' + data.school + '</div>';
-    content += '<div class="card_content">' + data.degree + " in " + data.major + '</div>';
+    content += '<div class="card_loc">' + data.location + '</div>';
+    content += '<div class="inner_card_date">' + data.from + " - " + data.to + '</div>';
+    content += '<div class="card_content">' + data.degree + ", " + data.major + '</div>';
     content += '</div>';
-
     content += '</div>';
 
     $('#education').find('.timeline_list').append(content);
 });
 
+data.internships.forEach(function (data, idx){
+    var logo = '<div class="card_logo_box">';
+    logo += '<div class="card_logo">';
+    // logo += '<img src="icons/logos/' + data.logo + '">';
+    logo += '</div>';
+    logo += '</div>';
 
+    var ls = '<ul>';
+    data.desc.forEach(function (item){
+        ls += '<li>' + item + '</li>';
+    });
+    ls += '</ul>'
 
-// var content = '<div class="timeline_item">';
+    var content = '<div class="timeline_item">';
+    content += '<div class="card_info">';
+    if(idx%2===0) content += logo;
+    content += '<div class="card_date">' + data.from + " - " + data.to + '</div>';
+    if(idx%2!==0) content += logo;
+    content += '</div>';
 
-// content += '<div class="card_info">';
-// content += '<div class="card_logo_box">' + data.school + '</div>';
-// content += '<div class="card_date">' + data.from + " - " + data.to + '</div>';
-// content += '</div>';
+    content += '<div class="card_item">';
+    content += '<div class="card_title">' + data.company + '</div>';
+    content += '<div class="card_loc">' + data.location + '</div>';
+    content += '<div class="inner_card_date">' + data.from + " - " + data.to + '</div>';
+    content += '<div class="card_content">' + ls + '</div>';
+    content += '</div>';
+    content += '</div>';
 
-// content += '<div class="card_item">';
-// content += '<div class="card_title">' + data.school + '</div>';
-// content += '<div class="card_content">' + data.degree + " in " + data.major + '</div>';
-// content += '</div>';
+    $('#internships').find('.timeline_list').append(content);
+});
 
-// content += '</div>';
+data.projects.forEach(function (data, idx){
+    var logo = '<div class="card_logo_box">';
+    logo += '<div class="card_logo">';
+    // logo += '<img src="icons/logos/' + data.logo + '">';
+    logo += '</div>';
+    logo += '</div>';
 
-// $('#education').find('.timeline_list').append(content);
+    var ls = '<ul>';
+    data.desc.forEach(function (item){
+        ls += '<li>' + item + '</li>';
+    });
+    ls += '</ul>'
 
+    var content = '<div class="timeline_item">';
+    content += '<div class="card_info">';
+    if(idx%2===0) content += logo;
+    content += '<div class="card_date">' + data.from + " - " + data.to + '</div>';
+    if(idx%2!==0) content += logo;
+    content += '</div>';
 
-// <div class="timeline_item">
-//     <div class="card_info">
-//         <div class="card_logo_box">2021</div>
-//         <div class="card_date">Season 13</div>
-//     </div>
-//     <div class="card_item">
-//         <div class="card_title">My test season for the title Season 13</div>
-//         <div class="card_content">
-//             Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula
-//             eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient
-//             montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu,
-//             pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla
-//             vel, aliquet nec, vulputate eget, arcu.
-//         </div>
-//     </div>
-// </div>
+    content += '<div class="card_item">';
+    content += '<div class="card_title">' + data.title + '</div>';
+    content += '<div class="inner_card_date">' + data.from + " - " + data.to + '</div>';
+    content += '<div class="card_content">' + ls + '</div>';
+    content += '</div>';
+    content += '</div>';
+
+    $('#projects').find('.timeline_list').append(content);
+}); 
