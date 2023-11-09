17e0582029a7c5761c1b51ff72cff125a5d06bd9
Yash Wardhan <yash1234sammie@gmail.com>
2023-11-09 01:49:53 -0500

    updated resume

--- data.js
+++ data.js
@@ -1,24 +1,36 @@
 data = {
-    "about": [],
+    "about": [
+        "👨‍💻 Computer Science Enthusiast | Data Analyst | Web Developer",
+        "🎓 Computer Science Graduate Student | 🎓 Bachelor's in Information Technology",
+        "🔍 Welcome to my Website! I'm a Computer Science graduate student with a solid foundation in Information Technology, passionate about leveraging technology to solve complex problems. Here's what you can expect from me:",
+        "🔬 Analytical & Problem-Solving Skills: Armed with a keen analytical mindset and a love for tackling challenges head-on, I thrive in dissecting complex problems and crafting innovative solutions.",
+        "🖥️ Core Computer Science Expertise: My academic journey has equipped me with a deep understanding of core computer science principles. From algorithms and data structures to database management, I'm well-versed in the foundations that power the digital world.",
+        "📊 Data Analytics Aficionado: I'm enthusiastic about harnessing the power of data to derive valuable insights. With proficiency in data analytics tools, I'm ready to turn raw data into actionable information.",
+        "💻 Web Development Wizard: Crafting user-friendly, dynamic web experiences is my forte. I'm proficient in the tools and languages essential for web development, ensuring that I can bring digital visions to life.",
+        "🚀 A Lifelong Learner: In this fast-evolving tech landscape, I'm always on the lookout for opportunities to upskill and keep pace with the latest trends. Learning isn't just a phase; it's a way of life.",
+        "🌟 Seeking Opportunities: I'm excited to connect with like-minded professionals, explore potential collaborations, and contribute to innovative projects. Let's explore the endless possibilities at the intersection of technology and creativity.",
+        "Feel free to reach out if you share similar interests or have exciting projects in mind. Together, we can make the digital world a better place."
+    ],
     "education":[
         {
             "school": "Rutgers, The State University of New Jersey",
             "location": "New Brunswick, New Jersey, USA",
-            "from": "Aug '22",
-            "to": "May '24",
+            "from": "September 2022",
+            "to": "May 2024",
             "degree": "Master of Science",
             "major": "Computer Science",
             "minor": "",
             "logo": "rutgers.png",
             "grade": "GPA: 3.67/4",
             "desc": [
+                "Business Intelligence and Visual Analytics", "Programming Finance", "Advanced Robotics",
                 "Operating Systems Design", "Database Management Systems", "Data Structures and Algorithms",
                 "Mathematical Foundations of Data Science", "Introduction to Artificial Intelligence", "Natural Language Processing", ],
         },{
             "school": "Manipal Institute of Technology",
             "location": "Manipal, Karnataka, India",
-            "from": "Jul '18",
-            "to": "Jun '22",
+            "from": "July 2018",
+            "to": "June 2022",
             "degree": "Bachelor of Technology",
             "major": "Information Technology",
             "minor": "Data Analytics",
@@ -27,8 +39,8 @@ data = {
         },{
             "school": "The Lawrence School, Sanawar",
             "location": "Solan, Himachal Pradesh, India",
-            "from": "Feb '09",
-            "to": "Dec '16",
+            "from": "February 2009",
+            "to": "December 2016",
             "degree": "Higher Secondary Education",
             "major": "PCM with Economics",
             "minor": "",
@@ -37,67 +49,88 @@ data = {
         }],
     "internships": [
         {
+            "company": "Acts Retirement-Life Communities",
+            "role": "Data Analyst Inter",
+            "location": "Fort Washington, PA",
+            "from": "June 2023",
+            "to": "August 2023",
+            "logo": "acts.jpg",
+            "desc": [
+                "Designed Power BI dashboards and reports featuring creative dynamic visuals and drill-through functionalities, facilitating more informed decision-making.",
+                "Developed procedures with Microsoft SQL Server Management Studio to extract JSON data from APIs, for storage in databases, facilitating subsequent analysis and utilization.",
+                "Executed data integration and transformation processes, fostering creation of impactful visual data narratives within the Power BI platform within a dynamic agile work environment.",
+            ],
+        },{
             "company": "TCS Digital",
             "role": "Frontend Developer Intern",
-            "location": "India (Remote)",
-            "from": "Mar '22",
-            "to": "Jun '22",
+            "location": "Gurugram, India",
+            "from": "March 2022",
+            "to": "June 2022",
             "logo": "tcs.jpg",
             "desc": ["Built web pages and functionalities to migrate from an outdated platform using Angular Material to incorporate Material Design that provides better UX and high-quality digital experience on the web."],
         },{
-            "company": "Rtwo Healthcare Solutions ",
+            "company": "Rtwo Healthcare Solutions",
             "role": "Software Engineer Intern",
-            "location": "Bangalore, Karnataka, India",
-            "from": "Jan '22",
-            "to": "May '22",
+            "location": "Bangalore, India",
+            "from": "January 2022",
+            "to": "May 2022",
             "logo": "rtwo.png",
             "desc":[
-                "Programmed full stack web solution using MERN stack to simplify patient admission and device allocation by employing IoT devices to interact with server, reducing human error.",
-                "Designed and deployed a network of esp32 microcontroller along with required API routes that establishes communication with server to facilitate data access."],
+                "Implemented a comprehensive full-stack web solution using MERN stack to streamline the patient admission process, resulting in a 40% improvement in work efficiency.",
+                "Programmed and deployed an integrated IoT device network with Rtwo’s software solution for device allocation, achieving a 20% reduction in manual labor.",
+                "Created crucial REST API routes to facilitate patient data retrieval and communication with RFID scanners for patient identification and allocation, eliminating manual data entry requirements."],
         },{
             "company": "Grroom",
-            "role": "Software Developer Intern",
-            "location": "India (Remote)",
-            "from": "May '21",
-            "to": "Jul '21",
+            "role": "Software Engineer Intern",
+            "location": "Mumbai, India",
+            "from": "May 2021",
+            "to": "July 2021",
             "logo": "grroom.png",
             "desc": [
-                "Constructed tailored recommendation algorithm to support search bar drop-down with product filters to deliver query results matching available catalogued products.",
-                "Implemented codes to automate data scraping for thousands of products from each website for listing on the app.",
+                " Constructed a search bar autocomplete recommendation feature within the app, efficiently suggesting products matching locally cataloged items, reducing response time from 3 seconds to just 0.2 seconds.",
+                " Developed Python scripts to automate data scraping tasks, extracting 1000s of product listings from 23 different clothing retail websites for seamless integration into the app.",
                 "Wrote programs to improve efficiency in manual data labeling and managed MongoDB database for training data."]
         }],
     "projects": [
         {
-            "title": "Twitter Database Search Application",
-            "from": "Mar '23",
-            "to": "Apr '23",
-            "desc":["Implemented a search application using Flask that stores Twitter data in MongoDB and MySQL allowing users to search tweets using various filters and strings along with a naive cache layer that improves search query time exponentially"]
+            "title": "Golf Ranger Application",
+            "from": "July 2023",
+            "to": "August 2023",
+            "desc":["Developed a React Native application aimed at assisting golf rangers in monitoring player time on the course, effectively maintaining the pace of play."]
+        },{
+            "title": "Twitter Search Application",
+            "from": "March 2023",
+            "to": "April 2023",
+            "desc":[
+                "Implemented a Flask-based search application utilizing MongoDB and MySQL, allowing users to search tweets.",
+                "Integrated a caching mechanism that significantly improved subsequent search response times by 70%."
+            ]
         },{
             "title": "Advanced Encryption Standard",
-            "from": "Oct '22",
-            "to": "Nov '22",
-            "desc":["Built an AES package having primary four functions and corresponding inverse functions performed in each round of AES encryption and decryption utilizing the key and other parameters taken from the user."]
+            "from": "October 2022",
+            "to": "November 2022",
+            "desc":["Wrote AES encryption and decryption functions, faithfully reproducing the core four operations and their corresponding inverses executed in each AES round, employing user-provided keys and parameters."]
         },{
-            "title": "Unix System Core Libraries' Implementation",
-            "from": "Oct '22",
-            "to": "Oct '22",
+            "title": "Unix System Libraries' Implementation",
+            "from": "October 2022",
+            "to": "October 2022",
             "desc":["Replicated the implementation of a file system and memory allocation that performs data manipulation in memory by creating custom functionalities for standard operations."]
         },{
-            "title": "Face Mask Detector",
-            "from": "Jun '21",
-            "to": "Jun '21",
-            "desc":["Constructed a model using YOLOv3 that categorizes individuals not wearing face masks in real time by transferring learned parameters and further training with a customized data set."]
+            "title": "Face Mask Detector Model",
+            "from": "June 2021",
+            "to": "June 2021",
+            "desc":["Developed a real-time face mask detection model using YOLOv3, fine-tuning pre-trained parameters, and enhancing its accuracy through custom dataset training."]
         },{
-            "title": "Travel Journal - Android and Web Application",
-            "from": "Feb '21",
-            "to": "May '21",
-            "desc":["Developed a cross-platform social media app using Android Studio, Node.js, and MongoDB enabling users to post personal data to track travel history"]
+            "title": "Travel Journal Application",
+            "from": "February 2021",
+            "to": "May 2021",
+            "desc":["Constructed a multi-platform social media application using Android Studio, Node.js, and MongoDB allowing users to share travel-related information and monitor their travel history."]
         },{
             "title": "Project Autonomous Underwater Vehicle Manipal",
-            "from": "May '21",
-            "to": "Aug '19",
+            "from": "August 2019",
+            "to": "May 2021",
             "desc":[
-                "Implemented ROS to enable AUV components to communicate with each other in required format with minimal latency and error, allowing components to collaborate and complete assigned tasks.",
+                "Utilized ROS (Robot Operating System) to establish seamless communication among AUV components, ensuring timely and efficient data exchange to support the successful completion of assigned tasks.",
                 "Studied the working of convolutional neural networks (CNN) and YOLO with computer vision team to implement a model for the detection and classification of objects underwater in real-time." ],
         }
     ],
