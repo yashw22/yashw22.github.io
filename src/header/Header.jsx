601a0feb2d078743990e29414d323662941bf813
Yash Wardhan <45715307+yashw22@users.noreply.github.com>
2024-12-17 20:32:20 -0500

    Name Logo

--- src/header/Header.jsx
+++ src/header/Header.jsx
@@ -1,5 +1,6 @@
 import { FaBars } from "react-icons/fa";
 import HeaderBtn from "./HeaderBtn";
+import NameLogo from "./NameLogo";
 import ThemeBtn from "./ThemeBtn";
 import { useState } from "react";
 
@@ -13,14 +14,14 @@ export default function Header() {
   ));
 
   return (
-    <div className="sticky top-2 z-50 px-5 py-2 mb-4 rounded-lg shadow-lg sm:px-10 lg:px-4 lg:mx-36 bg-light-bg/90 dark:bg-dark-bg/90 theme-anim">
-      <div className="flex items-center justify-between">
-        <div className="text-lg font-bold text-light-text dark:text-dark-text theme-anim">Yash Wardhan</div>
+    <>
+      <div className="header flex items-center justify-between">
+        <NameLogo name="Yash Wardhan" />
 
         <div className="flex items-center space-x-4 md:hidden">
           <ThemeBtn />
           <button onClick={() => setMenuOpen(!menuOpen)} className="p-2">
-            <FaBars className="w-6 h-6" />
+            <FaBars className="w-6 h-6 text-light-text dark:text-dark-text theme-anim" />
           </button>
         </div>
 
@@ -31,10 +32,10 @@ export default function Header() {
       </div>
 
       {menuOpen && (
-        <div className="mt-2 flex flex-col space-y-2 md:hidden">
+        <div className="fixed mt-2 right-10 flex flex-col items-end space-y-2 md:hidden">
           {getHeaderBtns}
         </div>
       )}
-    </div>
+    </>
   );
 }
---
+++ src/header/NameLogo.jsx
@@ -0,0 +1,47 @@
+import { useGSAP } from "@gsap/react";
+import { gsap } from "gsap";
+import PropTypes from "prop-types";
+import { useRef } from "react";
+
+export default function NameLogo({ name }) {
+  const fullName = name.split(" ");
+
+  const nameLogo = useRef(null);
+  const { contextSafe } = useGSAP({ scope: nameLogo });
+  const mouseEnter = contextSafe(() => {
+    gsap.to(".name", { width: "auto", duration: 0.5, ease: "power3.inOut" });
+  });
+  const mouseLeave = contextSafe(() => {
+    gsap.to(".name", { width: 0, duration: 0.5, ease: "power3.inOut" });
+  });
+
+  useGSAP(
+    () => {
+      gsap.to(".name", {
+        width: 0,
+        duration: 0.5,
+        ease: "power3.inOut",
+        delay: 2,
+      });
+    },
+    { scope: nameLogo }
+  );
+
+  return (
+    <div
+      className="text-2xl font-bold text-light-text dark:text-dark-text theme-anim flex grow group"
+      ref={nameLogo}
+      onMouseEnter={() => mouseEnter()}
+      onMouseLeave={() => mouseLeave()}
+    >
+      <div>{fullName[0][0]}</div>
+      <div className="name overflow-hidden">{fullName[0].slice(1)}</div>
+      <div>{fullName[1][0]}</div>
+      <div className="name overflow-hidden">{fullName[1].slice(1)}</div>
+    </div>
+  );
+}
+
+NameLogo.propTypes = {
+  name: PropTypes.string.isRequired,
+};
