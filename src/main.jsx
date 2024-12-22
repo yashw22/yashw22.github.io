b6e231e2483596b7e3e9752d988b3fa7356ad34d
Yash Wardhan <45715307+yashw22@users.noreply.github.com>
2024-12-21 20:25:07 -0500

    updated router

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,13 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import {
+  // createBrowserRouter,
+  // RouterProvider,
+  HashRouter,
+  Route,
+  Routes,
+} from "react-router-dom";
 import Layout from "./components/Layout";
 import NotFoundPage from "./Pages/NotFoundPage";
 import HeroPage from "./Pages/HeroPage";
@@ -10,22 +16,33 @@ import EducationPage from "./Pages/EducationPage";
 import InternshipPage from "./Pages/InternshipPage";
 import ProjectPage from "./Pages/ProjectPage";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Layout />,
-    children: [
-      { path: "/", element: <HeroPage /> },
-      { path: "/education", element: <EducationPage /> },
-      { path: "/internship", element: <InternshipPage /> },
-      { path: "/project", element: <ProjectPage /> },
-    ],
-  },
-  { path: "*", element: <NotFoundPage /> },
-]);
+// const router = createBrowserRouter([
+//   {
+//     path: "/",
+//     element: <Layout />,
+//     children: [
+//       { path: "/", element: <HeroPage /> },
+//       { path: "/education", element: <EducationPage /> },
+//       { path: "/internship", element: <InternshipPage /> },
+//       { path: "/project", element: <ProjectPage /> },
+//     ],
+//   },
+//   { path: "*", element: <NotFoundPage /> },
+// ]);
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <HashRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<HeroPage />} />
+          <Route path="education" element={<EducationPage />} />
+          <Route path="internship" element={<InternshipPage />} />
+          <Route path="project" element={<ProjectPage />} />
+        </Route>
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
+    </HashRouter>
+    {/* <RouterProvider router={router} /> */}
   </StrictMode>
 );
